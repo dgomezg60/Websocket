524f27f90b5d0d56ccd1e3e1464fbbb44276366a
David Gomez <75175977+dgomezg60@users.noreply.github.com>
2023-07-26 17:25:03 +0200

    Add files via upload

--- ClientClass.py
+++ ClientClass.py
@@ -1,5 +1,5 @@
 import asyncio
-from time import sleep
+import time
 import websockets
 import json
 
@@ -10,13 +10,14 @@ class client():
     async def __SendAuthenticationMessage(self):
         struct = r'{{"Token":"{}","ID":"{}"}}'
         FirstMessage = struct.format(self.token,self.Id)
-        await self.__clientListener.send(FirstMessage)
-        #await self.__clientSender.send(FirstMessage)
+        await asyncio.gather(self.__clientListener.send(FirstMessage),self.__clientSender.send(FirstMessage))
+        # await self.__clientListener.send(FirstMessage)
+        # await self.__clientSender.send(FirstMessage)
 
     async def connected(self,Token):     
         try:
-            self.__clientListener = await websockets.connect(f'ws://localhost:8765')
-            #self.__clientSender = await websockets.connect(f'ws://localhost:8766')
+            self.__clientListener = await websockets.connect(f'ws://192.168.0.16:8765')
+            self.__clientSender = await websockets.connect(f'ws://192.168.0.16:8766')
             self.token = Token
             await self.__SendAuthenticationMessage()
         except websockets.exceptions.ConnectionClosedError:
@@ -24,28 +25,30 @@ class client():
         except KeyboardInterrupt:
             print("\nDisconnect")
 
-    async def SendParameters(self,Position,Speed,Blocked,Queued,Status,ErrorStatus,BatteryLevel):
-    #async def SendParameters(self):
-        #struct = r'{{"IDRobot":"{}","Position":[0,0,0],"Speed":50,"Blocked":true,"Queued":true,"Status":"Enty","ErrorStatus":0,"BatteryLevel":20}}'
-        struct  = r'{{"IDRobot":"{}","Position":"{}","Speed":"{}","Blocked":"{}","Queued":"{}","Status":"{}","ErrorStatus":"{}","BatteryLevel":"{}"}}'
-        message = struct.format(self.Id,Position,Speed,Blocked,Queued,Status,ErrorStatus,BatteryLevel)
-        message = struct.format(self.Id)
-        await self.__clientSender.send(message)
+    #async def SendParameters(self,Position,Speed,Blocked,Queued,Status,ErrorStatus,BatteryLevel):
+    async def SendParameters(self):
+        while True:
+            struct = r'{{"IDRobot":"{}","Position":[0,0,0],"Speed":50,"Blocked":true,"Queued":true,"Status":"Enty","ErrorStatus":0,"BatteryLevel":20}}'
+            #struct  = r'{{"IDRobot":"{}","Position":"{}","Speed":"{}","Blocked":"{}","Queued":"{}","Status":"{}","ErrorStatus":"{}","BatteryLevel":"{}"}}'
+            #message = struct.format(self.Id,Position,Speed,Blocked,Queued,Status,ErrorStatus,BatteryLevel)
+            message = struct.format(self.Id)
+            await self.__clientSender.send(message)
+            await asyncio.sleep(1)
+
 
     async def ListeningServer(self):
-        Order = await self.__clientListener.recv()
-        self.message = json.loads(Order)
-        print("ID:{}, Position: {}".format(self.message['IDRobot'],self.message['Position']))
-        print(self.__clientListener)
-        sleep(1)
-        await self.ListeningServer()
+        while True:
+            print('Escucho')
+            Order = await self.__clientListener.recv()
+            self.message = json.loads(Order)
+            print("ID:{}, Position: {}".format(self.message['IDRobot'],self.message['Position']))
 
 async def main():
     Amr = client(0)
+    print('Me conecto')
     await Amr.connected("59")
     try:
-        #await Amr.SendParameters()
-        await Amr.ListeningServer()
+        await asyncio.gather(Amr.SendParameters(),Amr.ListeningServer())
     except KeyboardInterrupt:
         print('Disconnect')
         quit()
