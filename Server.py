64d0e4286461a4e85a1b8733498912f6d877d8ed
David Gomez <75175977+dgomezg60@users.noreply.github.com>
2023-07-27 17:22:53 +0200

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,15 +1,17 @@
 import asyncio
-from time import sleep
 import websockets
 import json
 
-ClientAuthorised = {}
-
-async def authenticate(FirstMessage,client):
+##-------------------------------------------------------------------Authentication Server----------------------------------------------------------------------------------------------
+##------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+async def authenticate(FirstMessage,client,typ):
     message = json.loads(FirstMessage)
     if message['Token'] == "59":
         authenticated = True
-        ClientAuthorised[message['ID']] = client
+        if typ == 1:
+            ClientAuthorisedListener[message['ID']] = client
+        if typ == 2:
+            ClientAuthorisedSender[message['ID']] = client
     else:
         authenticated = False
     if authenticated:
@@ -17,11 +19,12 @@ async def authenticate(FirstMessage,client):
     else:
         return False
 
-async def diconect(client):
+async def disconect(client):
     await client.close()
     print(f'Client disconected {client.id}')
     try:
-        _ = ClientAuthorised.pop(list(ClientAuthorised.keys())[list(ClientAuthorised.values()).index(client)])
+        _ = ClientAuthorisedSender.pop(list(ClientAuthorisedSender.keys())[list(ClientAuthorisedSender.values()).index(client)])
+        _ = ClientAuthorisedListener.pop(list(ClientAuthorisedListener.keys())[list(ClientAuthorisedListener.values()).index(client)])
     except ValueError:
         pass
 
@@ -35,55 +38,64 @@ async def read_message(client):
 async def server_handler_listen(client):
     authenticated = False
     auth_message = await client.recv()
-    authenticated = await authenticate(auth_message,client)
+    authenticated = await authenticate(auth_message,client,1)
     if authenticated:
         try:
             print(f'Client conected {client.id}')
             while True:
                 await read_message(client)
         except websockets.exceptions.ConnectionClosed:
-            await diconect(client)
+            await disconect(client)
     else:
         await client.close()
 
-##-------------------------------------------------------------------Server send----------------------------------------------------------------------------------------------
+##-------------------------------------------------------------------Server sender----------------------------------------------------------------------------------------------
 ##------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 async def send_message(Id):
     try:
-        client = ClientAuthorised[f'{Id}']
-        struct = r'{{"IDRobot":"{}","Position":[10,20,60]}}'
+        client = ClientAuthorisedSender[f'{Id}']
+        struct = r'{{"IDRobot":"{}","Position":[0,0,0]}}'
         message = struct.format(Id)
-        print(client)
         await client.send(message)
+        await asyncio.sleep(0.02)
     except KeyError:
         print(f'Cliend with ID {Id} doesnt exist')
 
 async def server_handler_send(client):
     authenticated = False
     auth_message = await client.recv()
-    authenticated = await authenticate(auth_message,client)
+    authenticated = await authenticate(auth_message,client,2)
     if authenticated:
         print(f'Client conected {client.id}')
         try:
-            while len(ClientAuthorised) != 0:
+            while len(ClientAuthorisedSender) != 0:
+                print('Do you want send a message? [y/n]')
+                Answer = input()
+                if Answer == 'y':
                     await send_message(0)
-                    sleep(1)
         except websockets.exceptions.ConnectionClosed:
-            await diconect(client)
+            await disconect(client)
     else:
         await client.close()
 
 ##------------------------------------------------------------------- Main ----------------------------------------------------------------------------------------------
 ##------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+async def WaitClosed(Server):
+    await Server.wait_closed()
+
 async def start_server(Ports):
-    server = await websockets.serve(server_handler_send, 'localhost', Ports[0])
-    print(f'Sending server turn on, at localhost:{Ports[0]}')
-    ListeningServer = await websockets.serve(server_handler_listen, 'localhost', Ports[1])
-    print(f'Listening server turn on, at localhost:{Ports[1]}')
-    await server.wait_closed()
+    global Ip
+    server = await websockets.serve(server_handler_send, Ip, Ports[0])
+    print(f'Sending server turn on, at {Ip}:{Ports[0]}')
+    ListeningServer = await websockets.serve(server_handler_listen, Ip , Ports[1])
+    print(f'Listening server turn on, at {Ip}:{Ports[1]}')
+    await asyncio.gather(WaitClosed(server),WaitClosed(ListeningServer))
 
 if __name__ == '__main__':
     Ports = [8765,8766]
+    Ip = '192.168.0.16'
+    ClientAuthorisedSender = {}
+    ClientAuthorisedListener = {}
     try:
         asyncio.run(start_server(Ports))
     except KeyboardInterrupt:
